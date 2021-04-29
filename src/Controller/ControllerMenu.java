3f5f949687b9659384adc5cbaf6907b0da5e8f46
August <AugustHauerslev@outlook.com>
2021-04-29 19:46:45 +0200

    extra

--- src/Controller/ControllerMenu.java
+++ src/Controller/ControllerMenu.java
@@ -1,16 +1,21 @@
 package Controller;
 
+import CreateUser.CreateAccount;
+
 public class ControllerMenu {
+
     public static void menu() {
         MenuText menuText = new MenuText();
         InputController input = new InputController();
+
+
         menuText.printMenuText();
 
 
         int choice = input.scanInt();
         switch (choice) {
             case 1 -> {
-                input.printString("You pressed 1");
+                input.printString("Enter Users");
                 menu();
             }
             case 2 -> {
---
+++ src/CreateUser/CreateAccount.java
@@ -0,0 +1,17 @@
+package CreateUser;
+
+public class CreateAccount {
+
+    public CreateAccount(){
+    }
+
+    public String createUserName(){
+        return "";
+    }
+
+    public String createPassword(){
+
+    }
+
+
+}
